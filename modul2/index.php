b0a496736584a902767f1f3b01bdf683ad52af4b
dioni3 <selimidion07@gmail.com>
2024-10-15 19:13:02 +0200

    Update index.php

--- modul2/index.php
+++ modul2/index.php
@@ -10,9 +10,15 @@ echo "$c \n"
 //sting Functions
 
 $the_string = 'Digital School';
-echo strlen(string: "$the_string\n")
+echo strlen(string: "$the_string\n");
 
 $prgramming = 'Programming is not cool';
-echo str_replace(search:"not")
+echo str_replace(search:"not", replace: "very" , subject: $programming);
+
+$another_string = 'Programming';
+
+echo strrev(string: $another_string);
+
+
 
 ?>
