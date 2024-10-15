a98c90795ac16842537f9ac7eed7edc5c3c95fc0
dioni3 <selimidion07@gmail.com>
2024-10-15 19:12:27 +0200

    Update index.php

---
+++ modul2/index.php
@@ -0,0 +1,18 @@
+<?
+echo "Hell world";
+$a = 'Digital';
+$b = 'School';
+
+$c = $a.$b;
+
+echo "$c \n"
+
+//sting Functions
+
+$the_string = 'Digital School';
+echo strlen(string: "$the_string\n")
+
+$prgramming = 'Programming is not cool';
+echo str_replace(search:"not")
+
+?>
